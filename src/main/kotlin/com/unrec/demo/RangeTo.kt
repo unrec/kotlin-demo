acbffe7bd4c60f7fd8308d3503fe64b1f034f712
unrec <andrei.troitskiy@gmail.com>
2021-07-20 17:29:15 +0300

    RangeTo.kt added

---
+++ src/main/kotlin/com/unrec/demo/RangeTo.kt
@@ -0,0 +1,17 @@
+package com.unrec.demo
+
+import java.time.LocalDate
+
+operator fun ClosedRange<LocalDate>.iterator(): Iterator<LocalDate> = object : Iterator<LocalDate> {
+    var current = start
+    override fun hasNext() = current <= endInclusive
+    override fun next() = current.apply { current = plusDays(1) }
+}
+
+fun main() {
+    val now = LocalDate.now()
+    val range = now..now.plusDays(10)
+    for (day in range) {
+        println(day)
+    }
+}
