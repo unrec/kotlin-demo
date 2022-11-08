d468f7e6949544b07eddadb5152c483be027cb5a
unrec <andrei.troitskiy@gmail.com>
2022-11-08 11:42:37 +0100

    Serialization.kt added

---
+++ src/main/kotlin/com/unrec/demo/Serialization.kt
@@ -0,0 +1,36 @@
+package com.unrec.demo
+
+import com.fasterxml.jackson.core.JsonParser
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import com.unrec.demo.GeometryType.SQUARE
+
+enum class GeometryType {
+    SQUARE,
+    CIRCLE,
+    POINT
+}
+
+data class GeometryItem(
+    val type: GeometryType,
+    val geometryJson: String
+)
+
+val json = """
+        {
+            type: "POINT",
+            geometryJson: "[12.0, 11.0]"
+        }
+    """.trimIndent()
+
+fun main() {
+    val mapper = jacksonObjectMapper()
+    mapper.configure(JsonParser.Feature.ALLOW_UNQUOTED_FIELD_NAMES, true)
+    
+    val geometryItem = GeometryItem(SQUARE, "[12.0, 11.0]")
+    val geometryItemSerialized = mapper.writeValueAsString(geometryItem)!!
+    println("geometryItemSerialized = $geometryItemSerialized")
+    
+    val geometryItemDeserialized = mapper.readValue<GeometryItem>(json)
+    println("geometryItemDeserialized = $geometryItemDeserialized")
+}
