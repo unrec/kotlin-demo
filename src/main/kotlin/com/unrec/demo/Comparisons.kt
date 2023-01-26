db2696df46ca52adfba3a2693c820b149c516d2a
unrec <andrei.troitskiy@gmail.com>
2023-01-26 18:29:57 +0100

    Comparisons.kt added

---
+++ src/main/kotlin/com/unrec/demo/Comparisons.kt
@@ -0,0 +1,20 @@
+package com.unrec.demo
+
+val items1 = listOf(
+    RatedItem(5.5f, "The Don", "John", 1984),
+    RatedItem(5.6f, "The House", "Jane", 1946),
+    RatedItem(7.8f, "The House", "Alice", 2002),
+)
+
+val items2 = listOf(
+    RatedItem(5.5f, "The Don", "John", 1984),
+    RatedItem(10.0f, "The Super Duper", "Director", 2022),
+    RatedItem(7.8f, "The House", "Alice", 2002),
+)
+
+fun main() {
+    val compareValuesBy = compareValuesBy(items1, items2) { it.size }
+    println("compareValuesBy = $compareValuesBy")
+
+}
+
